55ca6b5f450ea9fb6cff47a8e211022ea1f060fe
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-11-05 14:01:17 +0100

    Update PasswordResetEntity.java
    Added code for the PasswordReset entity

--- src/main/java/com/mindease/entity/PasswordResetEntity.java
+++ src/main/java/com/mindease/entity/PasswordResetEntity.java
@@ -1,5 +1,29 @@
 package com.mindease.entity;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.mindease.enums.RoleEnum;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordResetEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String resetToken; 
 }
