4cb24612e44dc0ceffb6caa1a58fcbbee510191b
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-11-06 22:34:55 +0100

    Update UserController.java
    I added import for the UserDTO record class

--- src/main/java/com/mindease/controllers/UserController.java
+++ src/main/java/com/mindease/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.mindease.controllers;
 
 import com.mindease.services.UserService;
+import com.mindease.DTO.UserDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
