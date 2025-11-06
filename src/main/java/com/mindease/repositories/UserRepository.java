1c146da4027c0e937026c6b20c0e6ff1c2eaddeb
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-11-06 18:19:09 +0100

    Update UserRepository.java
    Added UUID type instead of STRING

--- src/main/java/com/mindease/repositories/UserRepository.java
+++ src/main/java/com/mindease/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package com.mindease.repositories;
 import com.mindease.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByEmail(String email);
 
     Optional<UserEntity> findByEmail(String email);
