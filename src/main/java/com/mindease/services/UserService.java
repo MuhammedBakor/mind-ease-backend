8d0f6546f67b95d119a952222f92aa3bd51a20ce
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-11-06 23:25:04 +0100

    Update UserService.java

--- src/main/java/com/mindease/services/UserService.java
+++ src/main/java/com/mindease/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mindease.services;
 
 import com.mindease.repositories.UserRepository;
+import com.mindease.DTO.UserDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,26 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    
+   public Void updateProfile(UserDTO userReq){
+    if (userReq == null){
+        throw new IllegalArgunentException("no update information provided");
+    }
+       
+    // get email identity from security context
+    String email = SecurityContextHolder
+        .getContext()
+        .getAuthentication()
+        .getName();
 
+    // confirm if user exists in DB
+    UserEntity user = userRepository
+                     .findByEmail(email)
+                     .orElseThrow(() -> new Exception("user not found in"));
 
+    if (userReq.getFirstName != null){
+        userRepository.save(user.setFirstName(userReq.getFirstName());    
+   }
+
+    
 }
