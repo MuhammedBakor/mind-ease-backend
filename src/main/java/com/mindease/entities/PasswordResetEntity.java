60264e2cc0c643a7269b2e19896f6de168202ded
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-11-16 21:17:25 +0100

    Implement PasswordResetEntity with JPA annotations

--- src/main/java/com/mindease/entities/PasswordResetEntity.java
+++ src/main/java/com/mindease/entities/PasswordResetEntity.java
@@ -1,5 +1,25 @@
 package com.mindease.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class PasswordResetEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String resetToken;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
